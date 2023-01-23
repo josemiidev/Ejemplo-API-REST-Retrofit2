224490f61c1d7f6ed415308d24164c02ce2faf05
josemiidev-instituto <jlorlar1301@g.educaand.es>
2023-01-23 08:41:54 +0100

    Primer Commit

---
+++ app/src/main/java/com/example/ejemplo_api_rest_retrofit2/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.ejemplo_api_rest_retrofit2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
